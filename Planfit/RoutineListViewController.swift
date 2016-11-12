8efb937a35434a399c370b53cb35c49a70ce8bfa
Olya Sorokina <Olya@Olyas-MacBook-Air.local>
2016-11-11 21:11:05 -0800

    added view controller classes

---
+++ Planfit/RoutineListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RoutineListViewController.swift
+//  Planfit
+//
+//  Created by Olya Sorokina on 11/11/16.
+//  Copyright © 2016 Planfit. All rights reserved.
+//
+
+import UIKit
+
+class RoutineListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
